a626d01375f61d4dfa647006a5d1a1dbb1690f95
prha <prha@elementl.com>
2019-10-25 10:51:11 -0700

    skip tests, add 2.7 dependency

--- python_modules/dagster/dagster/core/execution/compute_logs.py
+++ python_modules/dagster/dagster/core/execution/compute_logs.py
@@ -38,9 +38,10 @@ def mirror_step_io(step_context):
 
 
 def should_disable_io_stream_redirect():
+    print(sys.platform, os.name)
     # See https://stackoverflow.com/a/52377087
     return (
-        sys.platform.startswith('win')
+        os.name == 'nt'
         and sys.version_info.major == 3
         and sys.version_info.minor >= 6
         and not os.environ.get('PYTHONLEGACYWINDOWSSTDIO')
